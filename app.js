054feceaff14d3ccff3a78d31874d9c25d76c0bf
Laurent Houssin <dylhunt46@gmail.com>
2023-01-15 11:51:33 +0100

    Creat route contoller and schema for users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
 mongoose
   .connect(
@@ -29,5 +30,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: 'Utilisateur crée' }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {};

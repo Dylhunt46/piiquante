0fe957f49cba0072c5f0ee373084aed848c182cd
Laurent Houssin <dylhunt46@gmail.com>
2023-01-31 16:56:08 +0100

    Modif models Sauce

--- models/Sauce.js
+++ models/Sauce.js
@@ -8,10 +8,10 @@ const sauceSchema = mongoose.Schema({
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { default: 0, type: Number },
-  dislikes: { default: 0, type: Number },
-  usersLiked: { type: [String] }, // + <userId>
-  usersDisliked: { type: [String] }, // + <userId>
+  likes: { type: Number, required: true, default: 0 },
+  dislikes: { type: Number, required: true, default: 0 },
+  usersLiked: { type: [String], required: true },
+  usersDisliked: { type: [String], required: true },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);

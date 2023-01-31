604739de020fa702e2371f4bfc217997f11ffad1
Laurent Houssin <dylhunt46@gmail.com>
2023-01-31 13:46:05 +0100

    Modif cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
 const path = require('path');
+
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');
+
 const app = express();
 
 mongoose.set('strictQuery', false);
 
+// Connexion base de données
 mongoose
   .connect(
     'mongodb+srv://Dylhunt:MonMdpCluster2912+@cluster0.iywoowj.mongodb.net/?retryWrites=true&w=majority',
@@ -15,22 +19,9 @@ mongoose
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Accès au corp de la requête
+// Middleware
+app.use(cors());
 app.use(express.json());
-
-//Ajout des headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  );
-  next();
-});
 
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);

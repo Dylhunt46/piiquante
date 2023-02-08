6e25b5ca4bba6f3da6de9bcfd19f4a0abc10e7df
Laurent Houssin <dylhunt46@gmail.com>
2023-02-08 18:25:26 +0100

    Add password-validator

---
+++ middleware/password.js
@@ -0,0 +1,35 @@
+const passwordValidator = require('password-validator');
+
+const passwordSchema = new passwordValidator();
+
+passwordSchema
+  .is()
+  .min(5)
+  .is()
+  .max(100)
+  .has()
+  .uppercase()
+  .has()
+  .lowercase()
+  .has()
+  .digits(1)
+  .has()
+  .not()
+  .spaces()
+  .is()
+  .not()
+  .oneOf(['Passw0rd', 'Password123']);
+
+console.log(passwordSchema);
+
+module.exports = (req, res, next) => {
+  if (passwordSchema.validate(req.body.password)) {
+    next();
+  } else {
+    return res.status(400).json({
+      error:
+        "Le mot de passe n'est pas assez fort :" +
+        passwordSchema.validate(req.body.password, { list: true }),
+    });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const password = require('../middleware/password');
 
 // Route pour créer un utilisateur
-router.post('/signup', userCtrl.signup);
+router.post('/signup', password, userCtrl.signup);
 // Route pour identifier un utilisateur quand il se connecte
 router.post('/login', userCtrl.login);
 
